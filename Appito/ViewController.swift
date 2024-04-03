e7321224a6806413c21d353940b9bcbce09c4609
GiorgiMM16 <michitashvili.giorgi3@gmail.com>
2024-04-04 00:35:11 +0300

    Initial Commit

---
+++ Appito/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Appito
+//
+//  Created by Giorgi Michitashvili on 4/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
